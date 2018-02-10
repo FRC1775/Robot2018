e99448761faf42710443729f3ccc9f9722133ed6
Andrew <bugiirobinson@gmail.com>
2018-02-10 00:06:27 +0000

    changed file

---
+++ src/org/usfirst/frc/team1775/robot/subsystems/LiftSubsystem.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team1775.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team1775.robot.Robot;
+import org.usfirst.frc.team1775.robot.OI;
+
+
+public class Intake extends Command{
+	
+    public Intake () {
+  anged  	requires(Robot.intakeSubsystem);
+    }
+    public void execute() {
+    double yVal2 = OI.myJoystick2.getRawAxis(1);
+    Robot.intakeSubsystem.setSpeed(yVal2);
+}
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
