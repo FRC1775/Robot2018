a2b31a63572faa31ad42518e705986146b1b7aa4
Tigerbytes 1775 Team Account <tigerbytes1775@gmail.com>
2018-02-19 16:11:41 -0600

    please work

--- src/org/usfirst/frc/team1775/robot/subsystems/MotorSubsystem.java
+++ src/org/usfirst/frc/team1775/robot/subsystems/MotorSubsystem.java
@@ -2,6 +2,8 @@ package org.usfirst.frc.team1775.robot.subsystems;
 
 import org.usfirst.frc.team1775.robot.RobotMap;
 import org.usfirst.frc.team1775.robot.commands.Drive;
+import org.usfirst.frc.team1775.robot.commands.SetMotorSpeed;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class MotorSubsystem extends Subsystem{
