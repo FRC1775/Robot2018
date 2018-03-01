3a88ca55ca7f806112aa3f270578915fb6fb82d2
tigerbytes1775 <tigerbytes1775@gmail.com>
2018-03-01 16:24:01 -0600

    commit just in case it wasn't committed last time

---
+++ src/org/usfirst/frc/team1775/robot/commands/autonomous/DriveToSwitchFromSides.java
@@ -0,0 +1,40 @@
+package org.usfirst.frc.team1775.robot.commands.autonomous;
+
+import org.usfirst.frc.team1775.robot.commands.DriveDistance;
+import org.usfirst.frc.team1775.robot.commands.RotateToAngle;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class DriveToSwitchFromSides extends CommandGroup {
+
+    public DriveToSwitchFromSides(int direction) {
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    	//One foot turning space
+    	addSequential(new DriveDistance(111));
+    	//Turning
+    	addSequential(new RotateToAngle(direction*90));
+    	//Brings robot back end to the edge of switch
+    	addSequential(new DriveDistance(55));
+    	
+
+    	
+    }
+}
