f5d500bd1de7643faa8f042131d9883d2f976273
tigerbytes1775 <tigerbytes1775@gmail.com>
2018-03-08 16:04:41 -0600

    added subsystem

---
+++ src/org/usfirst/frc/team1775/robot/subsystems/BlinkyLightSubsystem.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team1775.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class BlinkyLightSubsystem extends Subsystem {
+
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
+}
