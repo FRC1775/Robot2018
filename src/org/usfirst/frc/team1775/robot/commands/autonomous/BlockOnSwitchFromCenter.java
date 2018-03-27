2729a04af6dd1dbbbed58ae422289f495d61e150
Tigerbytes 1775 Team Account <tigerbytes1775@gmail.com>
2018-03-27 14:53:56 -0500

    added an autonomous start to BlockOnSwitchFromCenter

--- src/org/usfirst/frc/team1775/robot/commands/autonomous/BlockOnSwitchFromCenter.java
+++ src/org/usfirst/frc/team1775/robot/commands/autonomous/BlockOnSwitchFromCenter.java
@@ -1,6 +1,5 @@
 package org.usfirst.frc.team1775.robot.commands.autonomous;
 
-import org.usfirst.frc.team1775.robot.RobotMap;
 import org.usfirst.frc.team1775.robot.commands.DriveDistance;
 import org.usfirst.frc.team1775.robot.commands.FlippyCube;
 import org.usfirst.frc.team1775.robot.commands.LiftHeight;
@@ -11,7 +10,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class BlockOnSwitchFromCenter extends CommandGroup {
 
     public BlockOnSwitchFromCenter() {
-    	System.out.println("trying to put a block on switch from center");
+    	addSequential(new AutonomousStart());
 //    	addSequential(new DriveDistance((AutonomousConstants.AUTO_LINE - AutonomousConstants.ROBOT_LENGTH) +
 //    			(AutonomousConstants.ROBOT_LENGTH / 2)));
     	addSequential(new DriveToAutoLineFromCenter());
